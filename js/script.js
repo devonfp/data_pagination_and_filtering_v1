cdff7a0dedee4473d3a2781c8d1e27d55224f08e
Devon Poston <devonposton1@gmail.com>
2022-11-04 14:58:19 -0400

    Fixed showPage function & more
    Also added pagination function and event listener.

--- js/script.js
+++ js/script.js
@@ -20,27 +20,26 @@ This function will create and insert/append the elements needed to display a "pa
 
 function showPage(list, page) {
    const startIndex = (page * 9) -9;
-   const endIndex = (page * 9);
+   const endIndex = (page * 9) -1;
    
    const studentList = document.querySelector(".student-list"); 
    studentList.innerHTML = '';
-   for (let i = list; i < list.length; i++) {
-      if (i >= startIndex && i < endIndex) {
+   for (let i = 0; i < list.length; i++) {
+      if (i >= startIndex && i <= endIndex) {
          const studentItem = `<li class="student-item cf">
          <div class="student-details">
-           <img class="avatar" src="https://randomuser.me/api/portraits/women/25.jpg" alt="Profile Picture">
-           <h3>${[name].first.last}</h3>
-           <span class="email">${[email]}</span>
+           <img class="avatar" src="${list[i].picture.large}" alt="Profile Picture">
+           <h3>${list[i].name.first} ${list[i].name.last}</h3>
+           <span class="email">${list[i].email}</span>
          </div>
          <div class="joined-details">
-           <span class="date"> Joined ${[registered].date.age}</span>
+           <span class="date"> Joined ${list[i].registered.date}</span>
          </div>
        </li>`;
        studentList.insertAdjacentHTML('beforeend', studentItem);
-     } 
+     } //console.log(list[i])
     }
 }
-showPage(data, 1);
 
 
 
@@ -49,6 +48,35 @@ Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
 
+function addPagination(list) {
+ 
+   const numOfPages = Math.ceil(list.length/9);
+   const linkList =  document.querySelector('.link-list');
+   linkList.innerHTML = '';
 
+   for (let i = 1; i <= numOfPages; i++) {
+     const button = `<li>
+     <button type="button">${numOfPages}</button>
+   </li>`;
+   linkList.insertAdjacentHTML('beforeend', button);
+   }
+
+   const firstPagBtn = document.querySelector('button'); 
+   firstPagBtn.className = 'active';
+
+   linkList.addEventListener('click', (e) => {
+      
+      const eClicked = e.target;
+      
+      if (eClicked.tagName === 'BUTTON') {
+      firstPagBtn.className = '';
+      eClicked.className = 'active';
+      showPage(list, eClicked.textContent);
+   }
+
+ console.log(list);
+})}
 
 // Call functions
+showPage(data, 1);
+addPagination(data);
